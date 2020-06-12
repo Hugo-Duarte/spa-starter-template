6f5803a410c57f128166a5861ac516aff511c2ba
Hugo Duarte <HugoDuarte.dev@gmail.com>
2020-06-12 17:06:43 +0100

    Configured SPA, added vuetify base template and logout.

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,7 +21,7 @@ import Vuetify from "../plugins/vuetify";
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+Vue.component('app-container', require('./components/appContainer.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
